7e6d25a256c8034832ae2d2a1ffb2d15d024b7bf
thaind <thai.nd@yahoo.com>
2021-11-27 21:21:37 +0700

    Up

---
+++ QuyLe.Web.Editor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuyLe.Web.Editor.MvcApplication" Language="C#" %>
---
+++ QuyLe.Web.Editor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuyLe.Web.Editor.Startup))]
+namespace QuyLe.Web.Editor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
